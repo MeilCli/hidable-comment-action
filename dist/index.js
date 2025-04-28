755e2c6762c24deeeb8ff2c4092f33e310db95ad
MeilCli <2821921+MeilCli@users.noreply.github.com>
2025-04-28 23:03:26 +0000

    update actions

--- dist/index.js
+++ dist/index.js
@@ -8000,7 +8000,7 @@ function assertValidExecutionArguments(schema, document, rawVariableValues) {
  */
 
 function buildExecutionContext(args) {
-  var _definition$name, _operation$variableDe;
+  var _definition$name, _operation$variableDe, _options$maxCoercionE;
 
   const {
     schema,
@@ -8012,6 +8012,7 @@ function buildExecutionContext(args) {
     fieldResolver,
     typeResolver,
     subscribeFieldResolver,
+    options,
   } = args;
   let operation;
   const fragments = Object.create(null);
@@ -8074,7 +8075,14 @@ function buildExecutionContext(args) {
       ? rawVariableValues
       : {},
     {
-      maxErrors: 50,
+      maxErrors:
+        (_options$maxCoercionE =
+          options === null || options === void 0
+            ? void 0
+            : options.maxCoercionErrors) !== null &&
+        _options$maxCoercionE !== void 0
+          ? _options$maxCoercionE
+          : 50,
     },
   );
 
@@ -16526,10 +16534,7 @@ function visit(root, visitor, visitorKeys = _ast.QueryDocumentKeys) {
             }
           }
         } else {
-          node = Object.defineProperties(
-            {},
-            Object.getOwnPropertyDescriptors(node),
-          );
+          node = { ...node };
 
           for (const [editKey, editValue] of edits) {
             node[editKey] = editValue;
@@ -22425,7 +22430,10 @@ function coerceInputValueImpl(inputValue, type, onError, path) {
   }
 
   if ((0, _definition.isInputObjectType)(type)) {
-    if (!(0, _isObjectLike.isObjectLike)(inputValue)) {
+    if (
+      !(0, _isObjectLike.isObjectLike)(inputValue) ||
+      Array.isArray(inputValue)
+    ) {
       onError(
         (0, _Path.pathToArray)(path),
         inputValue,
@@ -25955,6 +25963,7 @@ class ValidationContext extends ASTValidationContext {
               node: variable,
               type: typeInfo.getInputType(),
               defaultValue: typeInfo.getDefaultValue(),
+              parentType: typeInfo.getParentInputType(),
             });
           },
         }),
@@ -30349,7 +30358,7 @@ function VariablesInAllowedPositionRule(context) {
       leave(operation) {
         const usages = context.getRecursiveVariableUsages(operation);
 
-        for (const { node, type, defaultValue } of usages) {
+        for (const { node, type, defaultValue, parentType } of usages) {
           const varName = node.name.value;
           const varDef = varDefMap[varName];
 
@@ -30377,6 +30386,21 @@ function VariablesInAllowedPositionRule(context) {
               context.reportError(
                 new _GraphQLError.GraphQLError(
                   `Variable "$${varName}" of type "${varTypeStr}" used in position expecting type "${typeStr}".`,
+                  {
+                    nodes: [varDef, node],
+                  },
+                ),
+              );
+            }
+
+            if (
+              (0, _definition.isInputObjectType)(parentType) &&
+              parentType.isOneOf &&
+              (0, _definition.isNullableType)(varType)
+            ) {
+              context.reportError(
+                new _GraphQLError.GraphQLError(
+                  `Variable "$${varName}" is of type "${varType}" but must be non-nullable to be used for OneOf Input Object "${parentType}".`,
                   {
                     nodes: [varDef, node],
                   },
@@ -30979,7 +31003,7 @@ exports.versionInfo = exports.version = void 0;
 /**
  * A string containing the version of the GraphQL.js library
  */
-const version = '16.10.0';
+const version = '16.11.0';
 /**
  * An object containing the components of the GraphQL.js version string
  */
@@ -30987,7 +31011,7 @@ const version = '16.10.0';
 exports.version = version;
 const versionInfo = Object.freeze({
   major: 16,
-  minor: 10,
+  minor: 11,
   patch: 0,
   preReleaseTag: null,
 });
